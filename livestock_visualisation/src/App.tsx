e68d5c2a152d64050eb5d033ffc5a13e3586fd9b
zhoujial1 <zhoujial1@myvuw.ac.nz>
2025-04-22 21:24:30 +1200

    Change the title and fix the layout formating

--- livestock_visualisation/src/App.tsx
+++ livestock_visualisation/src/App.tsx
@@ -37,13 +37,13 @@ function App() {
   }, [year, location, dataLoaded]);
 
   return (
-    <div className="min-h-screen bg-gray-100 p-4">
+    <div className="min-h-screen bg-gray-100 p-4 max-h-screen overflow-hidden">
       <h1 className="text-2xl font-bold mb-4">Design 1 - Walkthrough</h1>
       
       {/* Main container - converts to column on mobile */}
-      <div className="grid grid-cols-1 md:grid-cols-12 gap-4 h-auto md:h-[calc(100vh-100px)]">
+      <div className="grid grid-cols-1 md:grid-cols-12 gap-4 h-auto md:h-[calc(100vh-100px)] overflow-auto">
         {/* Left panel - Year selector and Section 1 */}
-        <div className="md:col-span-5 flex flex-col gap-4">
+        <div className="md:col-span-5 flex flex-col gap-4 max-h-[calc(100vh-120px)]">
           {/* Year selector */}
           <div className="bg-white rounded-lg shadow p-2 h-12">
             <select 
@@ -58,9 +58,9 @@ function App() {
           </div>
           
           {/* Section 1 - Main visualization */}
-          <div className="bg-white rounded-lg shadow h-[300px] md:flex-grow overflow-hidden">
+            <div className="bg-white rounded-lg shadow flex-grow overflow-auto">
             <Section1 year={year} location={location} setLocation={setLocation} />
-          </div>
+            </div>
           
           {/* Location selector */}
           <div className="bg-white rounded-lg shadow p-2 h-12">
@@ -77,7 +77,7 @@ function App() {
         </div>
         
         {/* Right panel - Sections 2, 3, and 4 */}
-        <div className="md:col-span-7 flex flex-col gap-4">
+        <div className="md:col-span-7 flex flex-col gap-4 max-h-[calc(100vh-120px)] overflow-auto">
           {/* Section 2 - Top visualization */}
           <div className="bg-white rounded-lg shadow h-[300px] md:h-1/2">
             <div className="p-2 bg-gray-100 text-center">Livestock Trends in {location}</div>
